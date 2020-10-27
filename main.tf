e2edb676081b86a8d48a152066093710cdfe8305
azymberdi <57076637+azymberdi@users.noreply.github.com>
2020-10-26 22:04:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,13 +23,13 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = "Z02097382TQD9L135RBW5"
-  name    = "www.turkmendevops.com"
-  type    = "A"
-  ttl     = "5"
-  records = ["${aws_instance.instance.public_ip}"]
-}
+#resource "aws_route53_record" "www" {
+  #zone_id = "Z02097382TQD9L135RBW5"
+  #name    = "www.turkmendevops.com"
+  #type    = "A"
+  #ttl     = "5"
+  #records = ["${aws_instance.instance.public_ip}"]
+#}
 
 resource "aws_security_group" "allow" {
   name        = "allow_tls_azymberdi"
