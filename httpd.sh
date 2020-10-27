bf1065079df246bdd820ca9552a1be07d15f00e7
azymberdi <57076637+azymberdi@users.noreply.github.com>
2020-10-26 22:04:42 -0400

    Update httpd.sh

--- httpd.sh
+++ httpd.sh
@@ -2,5 +2,5 @@
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo echo "Terraform is fun" > /var/www/html/index.html 
+sudo echo "PatraCorp is fun" > /var/www/html/index.html 
 sudo systemctl restart httpd
